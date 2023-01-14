dd202e5528b84a2fefbf649ab0a23d738922017d
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-15 04:27:13 +0700

    Import library to logger_test.go

--- logger_test.go
+++ logger_test.go
@@ -1,2 +1,11 @@
 // The package name.
 package gologging
+// Importing the packages that are needed for the program to run.
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
