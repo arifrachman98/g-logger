7e84c81912aa6b195cd3137dcd8eeaf43249a252
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-15 04:23:59 +0700

    Add some function to hook_test.go

--- hook_test.go
+++ hook_test.go
@@ -9,3 +9,17 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type SampleHook struct {
+}
+
+// Returning the levels that the hook will be called for.
+func (s *SampleHook) Levels() []logrus.Level {
+	return []logrus.Level{logrus.ErrorLevel, logrus.WarnLevel}
+}
+
+// A function that is called when the hook is fired.
+func (s *SampleHook) Fire(entry *logrus.Entry) error {
+	fmt.Println("Sample Hook", entry.Level, entry.Message)
+	return nil
+}
+
