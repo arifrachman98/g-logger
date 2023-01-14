45592cf74b5ffc19203f2e9159287df3b56ab64a
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-15 04:20:36 +0700

    Add new function test to singleton_test.go

--- singleton_test.go
+++ singleton_test.go
@@ -6,3 +6,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func TestSingletonLogging(t *testing.T) {
+	logrus.Info("Nope")
+	logrus.Warn("Nothing")
+
+	logrus.SetFormatter(&logrus.JSONFormatter{
+		PrettyPrint: true,
+	})
+
+	logrus.Info("Duarrrr!!!")
+}
