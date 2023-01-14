cd8ee7cecc2b19595b6952577abd1d665c660916
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-15 04:28:13 +0700

    Add new function TestLog to logger_test.go

--- logger_test.go
+++ logger_test.go
@@ -9,3 +9,11 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func TestLog(t *testing.T) {
+	// Creating a new instance of the logger.
+	logger := logrus.New()
+
+	// Printing the message to the console.
+	logger.Println("Hello logger sirupsen")
+	fmt.Println("Hello logger Go-Lang")
+}
